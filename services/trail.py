8f1524177a4d23cf7ae59c863566ffd34345abe3
Vickonweb3 <vnnamdi534@gmail.com>
2026-08-24 07:11:13 +0100

    Delete services directory

--- services/trail.py
+++
@@ -1,9 +0,0 @@
-# Trail service
-# Manages influencer trails and tracking
-
-class TrailService:
-    """Service for managing influencer trails."""
-    
-    def create_trail(self, influencer_id):
-        """Create a new trail."""
-        pass
